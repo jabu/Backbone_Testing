0174e78d162c456662bec481d5ca43361b4a672a
Jabulani Vilakazi <befree2j@gmail.com>
2017-07-12 09:09:29 +0200

    Added Unit test for controller methods

--- Backbone_Testing.Tests/UnitTest1.cs
+++ Backbone_Testing.Tests/UnitTest1.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BackboneDL;
+using System.Net;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Routing;
+using System.Web.Http.Controllers;
+using System.Web.Http.Hosting;
+using Backbone_Testing.Controllers;
+using System.Web.Http.Results;
 
 namespace Backbone_Testing.Tests
 {
@@ -8,23 +16,60 @@ namespace Backbone_Testing.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UpdateEmployeeTest()
         {
+            
+            var controller = new EmployeeController();
+            Employee employee = new Employee { Id = 1606, Name = "Demo1", KnownAs = "Demo1", Surname = "Demo1", Position = "Demo1", ContactNumber = "Demo1" };
 
+            int Result = controller.Put(1606, employee);            
+            Assert.IsNotNull(Result);            
         }
 
         [TestMethod]
         public void GetAllEmployees_ShouldReturnAllEmployees()
         {
-            var testEmployees = GetTestProducts();
+            System.Collections.Generic.List<Employee> testEmployees = GetTestEmployees();
             var controller = new Controllers.EmployeeController();
 
-            var result = controller.Get() as System.Collections.Generic.List<Employee>;
-            Assert.AreEqual(testEmployees.Count, result.Count);
+            System.Collections.Generic.List<Employee> result = controller.Get() as System.Collections.Generic.List<Employee>;
+            Assert.AreNotSame(testEmployees[0], result[1]);
         }
 
         [TestMethod]
-        private System.Collections.Generic.List<Employee> GetTestProducts()
+        public void GetEmployeFound()
+        {
+            // Set up Prerequisites   
+            var controller = new EmployeeController();
+            // Act  
+            Employee employee = controller.Get(1606);
+            // Assert  
+            Assert.IsNotNull(employee);
+        }
+
+        [TestMethod]
+        public void GetAllEmployees_ShouldReturnEmployee()
+        {
+            System.Collections.Generic.List<Employee> testEmployees = GetTestEmployees();
+            var controller = new Controllers.EmployeeController();
+
+            System.Collections.Generic.List<Employee> result = controller.Get();
+            
+            Assert.AreNotEqual(testEmployees[1], result[0]);
+        }
+
+        [TestMethod]
+        public void AddEmployee()
+        {
+            var controller = new Controllers.EmployeeController();
+            var employee = new Employee { Id = 1, Name = "Demo1", KnownAs = "Demo1", Surname = "Demo1", Position = "Demo1", ContactNumber = "Demo1" };
+            Employee newEmployee = controller.Post(employee);
+            Employee emp = new Employee();
+            Assert.AreNotSame(emp, newEmployee);
+        }
+
+        [TestMethod]
+        private System.Collections.Generic.List<Employee> GetTestEmployees()
         {
             var testEmployees = new System.Collections.Generic.List<Employee>();
             testEmployees.Add(new Employee { Id = 1, Name = "Demo1", KnownAs = "Demo1", Surname = "Demo1", Position = "Demo1", ContactNumber = "Demo1" });
@@ -56,7 +101,7 @@ namespace Backbone_Testing.Tests
                 ContactNumber = "Demo4"
             });
 
-            return testEmployees;
+            return testEmployees as System.Collections.Generic.List<Employee>;
         }
     }
 }
